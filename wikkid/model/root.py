e0739e2315f28983eb25bd97f30bab8b8972c2df
Tim Penhey <tim@penhey.net>
2010-04-17 22:39:48 +1200

    The root resource model class.

---
+++ wikkid/model/root.py
@@ -0,0 +1,37 @@
+#
+# Copyright (C) 2010 Wikkid Developers
+#
+# This file is part of Wikkid.
+#
+# Wikkid is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Foobar is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Wikkid.  If not, see <http://www.gnu.org/licenses/>
+
+"""The root resource class.
+
+The root resource represents the object at the root of the wiki path.
+
+Currently this just refers to '/', but it is expected that at some stage in
+the not too distant future the server will support a wiki root where it is not
+the root path.
+"""
+
+from zope.interface import implements
+
+from wikkid.model.baseresource import BaseResource
+from wikkid.interface.resource import IRootResource
+
+
+class RootResource(BaseResource):
+    """Information about a resource."""
+
+    implements(IRootResource)
