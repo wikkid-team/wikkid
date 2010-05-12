d6808d0d4a73d7aeda651dedeb7a76a49b57872c
Tim Penhey <tim@penhey.net>
2010-05-12 22:39:13 +1200

    Change the license to AGPL3.

--- wikkid/model/baseresource.py
+++ wikkid/model/baseresource.py
@@ -1,20 +1,8 @@
 #
-# Copyright (C) 2010 Wikkid Developers
+# Copyright (C) 2010 Wikkid Developers.
 #
-# This file is part of Wikkid.
-#
-# Wikkid is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Foobar is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Wikkid.  If not, see <http://www.gnu.org/licenses/>
+# This software is licensed under the GNU Affero General Public License
+# version 3 (see the file LICENSE).
 
 """The base resource class."""
 
