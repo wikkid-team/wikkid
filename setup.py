b21124c6e499ac74b9104ce7c4ecb807c0b2b2d2
Tim Penhey <tim@penhey.net>
2010-05-29 16:12:21 +1200

    Add a central version string.

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from wikkid import version
 
 setup(name='Wikkid',
-      version='0.1',
+      version=version,
       description="A wiki that is backed by Bazaar that allows local branching of the wiki for later merging. Also doesn't have any page locks and uses Bazaar's three way merging.",
       author='Wikkid Developers',
       author_email='wikkid-dev@lists.launchpad.net',
