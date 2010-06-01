a47a50896c9ac5106f20344d0f02b5cb2391d2b0
Martin Albisetti <martin.albisetti@canonical.com>
2010-06-01 07:20:55 +0200

    Use expanded wiki name instead of the plain base name, per Tim's suggestion.

--- wikkid/view/edit.py
+++ wikkid/view/edit.py
@@ -7,6 +7,7 @@
 """View base class for editing text pages."""
 
 from wikkid.view.base import BaseView
+from wikkid.view.utils import expand_wiki_name
 
 
 class BaseEditView(BaseView):
@@ -17,7 +18,7 @@ class BaseEditView(BaseView):
 
     @property
     def title(self):
-        return 'Editing "%s"' % self.context.base_name
+        return 'Editing "%s"' % expand_wiki_name(self.context.base_name)
 
     @property
     def cancel_url(self):
