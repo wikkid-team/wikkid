b9aa49a1e310cd08f9896007d2e4e2b7bee6564d
Tim Penhey <tim@penhey.net>
2010-06-15 21:17:40 +1200

    More tests.

--- wikkid/tests/views/test_urls.py
+++ wikkid/tests/views/test_urls.py
@@ -28,3 +28,60 @@ class TestCanonicalUrl(FactoryTestCase):
         root = factory.get_resource_at_path('/')
         self.assertEqual('/+listing', canonical_url(root, 'listing'))
 
+    def test_default(self):
+        factory = self.make_factory()
+        root = factory.get_resource_at_path('/')
+        self.assertEqual('/Home', canonical_url(root.default_resource))
+
+    def test_default_view(self):
+        factory = self.make_factory()
+        root = factory.get_resource_at_path('/')
+        self.assertEqual(
+            '/Home/+edit',
+            canonical_url(root.default_resource, 'edit'))
+
+    def test_wiki_page(self):
+        factory = self.make_factory([
+            ('SomeDir.txt', 'Some content'),
+            ])
+        page = factory.get_resource_at_path('/SomeDir')
+        self.assertEqual('/SomeDir', canonical_url(page))
+
+    def test_wiki_page_view(self):
+        factory = self.make_factory([
+            ('SomeDir.txt', 'Some content'),
+            ])
+        page = factory.get_resource_at_path('/SomeDir')
+        self.assertEqual('/SomeDir/+edit', canonical_url(page, 'edit'))
+
+    def test_wiki_page_full_url(self):
+        factory = self.make_factory([
+            ('SomeDir.txt', 'Some content'),
+            ])
+        page = factory.get_resource_at_path('/SomeDir.txt')
+        self.assertEqual('/SomeDir', canonical_url(page))
+
+    def test_wiki_page_full_url_with_view(self):
+        factory = self.make_factory([
+            ('SomeDir.txt', 'Some content'),
+            ])
+        page = factory.get_resource_at_path('/SomeDir.txt')
+        self.assertEqual('/SomeDir/+edit', canonical_url(page, 'edit'))
+
+    def test_other_file(self):
+        factory = self.make_factory([
+            ('simple.py', '#!/usr/bin/python'),
+            ])
+        page = factory.get_resource_at_path('/simple.py')
+        self.assertEqual('/simple.py', canonical_url(page))
+
+    def test_other_file_view(self):
+        factory = self.make_factory([
+            ('simple.py', '#!/usr/bin/python'),
+            ])
+        page = factory.get_resource_at_path('/simple.py')
+        self.assertEqual('/simple.py/+edit', canonical_url(page, 'edit'))
+
+
+
+
