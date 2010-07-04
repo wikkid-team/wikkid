3cc2e1b6c469b8493e78dda8fe2f20e5f4607354
Gavin Panella <gavin@gromper.net>
2010-07-04 18:23:39 +0100

    A variation of test_traverse_above_static_not_possible with an absolute path.

--- wikkid/tests/test_app.py
+++ wikkid/tests/test_app.py
@@ -6,6 +6,7 @@
 
 """Tests for method and classes in wikkid.app."""
 
+import os.path
 
 from wikkid.app import WikkidApp
 from wikkid.filestore.volatile import FileStore
@@ -14,7 +15,7 @@ from wikkid.tests import TestCase
 
 class TestApp(TestCase):
 
-    def test_traverse_above_static_not_possible(self):
+    def test_traverse_above_static_not_possible_with_relative_path(self):
         """
         Traversal above the static folder, by forging a malicious request for
         example, is not possible.
@@ -22,6 +23,24 @@ class TestApp(TestCase):
         environ = {
             "REQUEST_METHOD": "GET",
             "PATH_INFO": "/static/../page.html",
+            }
+
+        def start_response(status, headers):
+            self.assertEqual("404 Not Found", status)
+
+        filestore = FileStore()
+        app = WikkidApp(filestore)
+        app(environ, start_response)
+
+    def test_traverse_above_static_not_possible_with_absolute_path(self):
+        """
+        Traversal above the static folder, by forging a malicious request for
+        example, is not possible.
+        """
+        this_file = os.path.abspath(__file__)
+        environ = {
+            "REQUEST_METHOD": "GET",
+            "PATH_INFO": "/static/" + this_file,
             }
 
         def start_response(status, headers):
