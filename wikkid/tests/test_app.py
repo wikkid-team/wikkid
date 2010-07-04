da07d423206ec45e77f686519bdae5d0f5b10dd9
Gavin Panella <gavin@gromper.net>
2010-07-04 18:20:36 +0100

    Test case for bug 601612, where it's possible to traverse above the static content directory.

--- wikkid/tests/test_app.py
+++ wikkid/tests/test_app.py
@@ -6,3 +6,27 @@
 
 """Tests for method and classes in wikkid.app."""
 
+
+from wikkid.app import WikkidApp
+from wikkid.filestore.volatile import FileStore
+from wikkid.tests import TestCase
+
+
+class TestApp(TestCase):
+
+    def test_traverse_above_static_not_possible(self):
+        """
+        Traversal above the static folder, by forging a malicious request for
+        example, is not possible.
+        """
+        environ = {
+            "REQUEST_METHOD": "GET",
+            "PATH_INFO": "/static/../page.html",
+            }
+
+        def start_response(status, headers):
+            self.assertEqual("404 Not Found", status)
+
+        filestore = FileStore()
+        app = WikkidApp(filestore)
+        app(environ, start_response)
