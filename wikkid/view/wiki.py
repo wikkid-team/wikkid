5c6e045ce3313ebe67cc13b632f0f7892fae9922
Henning Eggers <henning@canonical.com>
2010-10-19 23:23:03 +0200

    Extract formatting code.

--- wikkid/view/wiki.py
+++ wikkid/view/wiki.py
@@ -12,6 +12,17 @@ from wikkid.formatter.registry import get_wiki_formatter
 from wikkid.interface.resource import IWikiTextFile
 from wikkid.view.base import BaseView
 
+def format_context(context):
+    """ Format the context with the right formatter.
+
+    Check the first line of the content to see if it specifies a
+    formatter. The default is currently ReST, but we should have it
+    configurable shortly.
+    """
+    bytes = context.get_bytes()
+    content, formatter = get_wiki_formatter(bytes, 'rest')
+    return formatter.format(context.base_name, content)
+    
 
 class WikiPage(BaseView):
     """A wiki page is a page that is going to be rendered for viewing."""
@@ -23,12 +34,7 @@ class WikiPage(BaseView):
 
     @property
     def content(self):
-        bytes = self.context.get_bytes()
-        # Check the first line of the content to see if it specifies a
-        # formatter. The default is currently ReST, but we should have it
-        # configurable shortly.
-        content, formatter = get_wiki_formatter(bytes, 'rest')
-        return formatter.format(self.context.base_name, content)
+        return format_context(self.context)
 
     def _render(self, skin):
         """If the page is not being viewed with the preferred path, redirect.
