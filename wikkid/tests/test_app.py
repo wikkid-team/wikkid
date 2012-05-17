dcc3736702b47a3023b2fee4377c1bb5d731f0ee
Tim Penhey <tim@penhey.net>
2012-05-17 20:41:00 +1200

    Commit an actual passing test.

--- wikkid/tests/test_app.py
+++ wikkid/tests/test_app.py
@@ -45,3 +45,14 @@ class TestApp(TestCase):
         filestore = FileStore()
         app = WikkidApp(filestore)
         app(environ, start_response)
+
+    def test_getting_static_style_css_works(self):
+
+        environ = environ_from_url("/static/default.css")
+
+        def start_response(status, headers):
+            self.assertEqual("200 OK", status)
+
+        filestore = FileStore()
+        app = WikkidApp(filestore)
+        app(environ, start_response)
