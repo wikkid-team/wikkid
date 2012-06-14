fb1795dbbc91fff895e174b40c95d8f60d1b0c4a
Tim Penhey <tim@penhey.net>
2012-06-14 20:41:06 +1200

    Update header.

--- wikkid/tests/test_app.py
+++ wikkid/tests/test_app.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2010 Wikkid Developers.
+# Copyright (C) 2010-2012 Wikkid Developers.
 #
 # This software is licensed under the GNU Affero General Public License
 # version 3 (see the file LICENSE).
